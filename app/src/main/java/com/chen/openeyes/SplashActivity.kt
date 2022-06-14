4fc00b4c51129d3e35e011422b778aad6ba77048
Mr_XiaoChen <1816132309@qq.com>
2022-06-14 22:22:28 +0800

    d

--- app/src/main/java/com/chen/openeyes/SplashActivity.kt
+++ app/src/main/java/com/chen/openeyes/SplashActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * 启动页
+ */
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
